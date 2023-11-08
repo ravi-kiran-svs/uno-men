1cd4671b7f5b5bf05331359e68cdef5cf3359f83
i_rk_jain <svsravikiran.9@gmail.com>
2023-11-08 22:26:12 +0530

    minor bug fix
    Card Send animation jitter fixed.

--- Scripts/UI/CardSendAnim/CardSendAnimService.gd
+++ Scripts/UI/CardSendAnim/CardSendAnimService.gd
@@ -10,6 +10,7 @@ var _offset = Vector2(-32, -32)
 func play(from : Vector2, to : Vector2):
 	if	!tween.is_active():
 		cardView.visible = true
+		cardView.rect_global_position = from + _offset
 		
 		tween.interpolate_property(cardView, "rect_global_position", 
 		from + _offset, to + _offset, tTween, Tween.TRANS_EXPO, Tween.EASE_OUT)
